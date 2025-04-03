5c433066f8a3eda1c45d95ebc4d998997fcdb971
Davi Miranda <davimiranda210@gmail.com>
2025-04-03 08:11:19 -0300

    Adiciona submódulo T197_N24CD_Rafael_Leites

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>njfdfewnfwenfekfwlfwke</Text>
+      <Text>Aononeofnew</Text>
+      
+      <StatusBar style="auto" />
+
+
+
+
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
